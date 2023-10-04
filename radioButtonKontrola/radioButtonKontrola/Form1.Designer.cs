e6f07f1ff21a75ad881715f841e0c1cb41b8c5e1
MatejLukasMarunica <145869520+MatejLukasMarunica@users.noreply.github.com>
2023-10-04 08:43:01 +0200

    Add files via upload

---
+++ radioButtonKontrola/radioButtonKontrola/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace radioButtonKontrola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonAvion = new System.Windows.Forms.RadioButton();
+            this.radioButtonAutobus = new System.Windows.Forms.RadioButton();
+            this.radioButtonVlastiti = new System.Windows.Forms.RadioButton();
+            this.rezerviraj = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonVlastiti);
+            this.groupBox1.Controls.Add(this.radioButtonAutobus);
+            this.groupBox1.Controls.Add(this.radioButtonAvion);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox1.Location = new System.Drawing.Point(235, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 161);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Odaberite vrstu prijevoza";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // radioButtonAvion
+            // 
+            this.radioButtonAvion.AutoSize = true;
+            this.radioButtonAvion.Location = new System.Drawing.Point(6, 39);
+            this.radioButtonAvion.Name = "radioButtonAvion";
+            this.radioButtonAvion.Size = new System.Drawing.Size(62, 22);
+            this.radioButtonAvion.TabIndex = 0;
+            this.radioButtonAvion.TabStop = true;
+            this.radioButtonAvion.Text = "Avion";
+            this.radioButtonAvion.UseVisualStyleBackColor = true;
+            this.radioButtonAvion.CheckedChanged += new System.EventHandler(this.radioButtonAvion_CheckedChanged);
+            // 
+            // radioButtonAutobus
+            // 
+            this.radioButtonAutobus.AutoSize = true;
+            this.radioButtonAutobus.Location = new System.Drawing.Point(6, 79);
+            this.radioButtonAutobus.Name = "radioButtonAutobus";
+            this.radioButtonAutobus.Size = new System.Drawing.Size(80, 22);
+            this.radioButtonAutobus.TabIndex = 1;
+            this.radioButtonAutobus.TabStop = true;
+            this.radioButtonAutobus.Text = "Autobus";
+            this.radioButtonAutobus.UseVisualStyleBackColor = true;
+            this.radioButtonAutobus.CheckedChanged += new System.EventHandler(this.radioButtonAutobus_CheckedChanged);
+            // 
+            // radioButtonVlastiti
+            // 
+            this.radioButtonVlastiti.AutoSize = true;
+            this.radioButtonVlastiti.Location = new System.Drawing.Point(6, 119);
+            this.radioButtonVlastiti.Name = "radioButtonVlastiti";
+            this.radioButtonVlastiti.Size = new System.Drawing.Size(123, 22);
+            this.radioButtonVlastiti.TabIndex = 2;
+            this.radioButtonVlastiti.TabStop = true;
+            this.radioButtonVlastiti.Text = "Vlastiti prijevoz";
+            this.radioButtonVlastiti.UseVisualStyleBackColor = true;
+            this.radioButtonVlastiti.CheckedChanged += new System.EventHandler(this.radioButtonVlastiti_CheckedChanged);
+            // 
+            // rezerviraj
+            // 
+            this.rezerviraj.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.rezerviraj.Location = new System.Drawing.Point(241, 232);
+            this.rezerviraj.Name = "rezerviraj";
+            this.rezerviraj.Size = new System.Drawing.Size(194, 46);
+            this.rezerviraj.TabIndex = 1;
+            this.rezerviraj.Text = "Rezerviraj";
+            this.rezerviraj.UseVisualStyleBackColor = true;
+            this.rezerviraj.Click += new System.EventHandler(this.rezerviraj_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(241, 306);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 77);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.rezerviraj);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Upotreba Radio Button Kontrole";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonVlastiti;
+        private System.Windows.Forms.RadioButton radioButtonAutobus;
+        private System.Windows.Forms.RadioButton radioButtonAvion;
+        private System.Windows.Forms.Button rezerviraj;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ radioButtonKontrola/radioButtonKontrola/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace radioButtonKontrola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButtonAvion_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButtonAutobus_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButtonVlastiti_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rezerviraj_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+
+            textBox1.AppendText("Odabrali ste prijevoz: " + "\r\n");
+
+            if(radioButtonAvion.Checked)
+            {
+                textBox1.AppendText("Avionom ");
+            }
+            if (radioButtonAutobus.Checked)
+            {
+                textBox1.AppendText("Autobusom ");
+            }
+            if (radioButtonVlastiti.Checked)
+            {
+                textBox1.AppendText("Vlastitim prijevozom ");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
